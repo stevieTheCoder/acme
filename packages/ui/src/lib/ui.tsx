443c3002c675c7da91baefc55592b595e370eb55
Steve Rogers <steven.rogers@artlist.io>
2025-07-17 14:52:16 +0100

    add hero component

---
+++ packages/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function AcmeUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to AcmeUi!</h1>
+    </div>
+  );
+}
+
+export default AcmeUi;
